7d5b17eda9d7cca2b56270c92a8ac86206311cc0
Ayan Ali <ayanali.khi2@gmail.com>
2025-05-21 15:24:17 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 
 MQTT_BROKER = "localhost"
 MQTT_PORT = 1883
-MQTT_TOPIC = "home/sensors/temperature"
+MQTT_TOPIC = "your mqtt topics"
 
 MYSQL_HOST = "localhost"
 MYSQL_USER = "root"
-MYSQL_PASSWORD = "alhanali123$"
+MYSQL_PASSWORD = "your_password"
 MYSQL_DB = "smart_home"
 
 MONGO_URI = "mongodb://localhost:27017/"
@@ -14,4 +14,4 @@ MONGO_DB = "smart_home"
 
 NEO4J_URI = "bolt://localhost:7687"
 NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "alhanali123"
+NEO4J_PASSWORD = "your_password"
